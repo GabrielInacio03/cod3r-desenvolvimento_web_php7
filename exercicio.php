728b61b68115ff4a43f74d0a205d2c8ac49f1532
GabrielInacio03 <inaciogs03@gmail.com>
2022-04-21 17:50:54 -0300

    layout

---
+++ exercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cod3r - Exercício</title>
+
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+
+    <!-- MEU CSS -->
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+</head>
+<body>   
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Vizualização do exercício</h2>
+        
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS - <?php echo date('Y'); ?>
+    </footer>
+</body>
+</html>
