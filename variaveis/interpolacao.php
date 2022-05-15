cdff62474051412c218f33fb273b75c1fc25c355
GabrielInacio03 <inaciogs03@gmail.com>
2022-05-15 18:23:07 -0300

    interpolação

---
+++ variaveis/interpolacao.php
@@ -0,0 +1,8 @@
+<div class="titulo">Interpolação</div>
+
+<?php
+    //Interpolação => interpretar o valor de uma variável atraves de um contexto, como uma string.
+    $numero = 'numero';
+    echo $numero;
+    echo "<br> $numero";
+    echo "<br> eu tenho 10 {$numero}s";
