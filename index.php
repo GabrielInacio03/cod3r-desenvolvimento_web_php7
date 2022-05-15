d5d55f5811916b44ad6116da1454a76e0d7d7939
GabrielInacio03 <inaciogs03@gmail.com>
2022-05-15 18:57:23 -0300

    variáveis variáveis

--- index.php
+++ index.php
@@ -87,6 +87,9 @@
                         <li>
                             <a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=variaveldevariavel">Variáveis Variáveis</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
---
+++ variaveis/variaveldevariavel.php
@@ -0,0 +1,7 @@
+<div class="titulo">Variáveis Variáveis</div>
+<?php
+    //variáveis com nome de variável
+
+    $a = 'valorA';
+    $$a = 'valorAA';
+    echo "$a {$valorA}";
